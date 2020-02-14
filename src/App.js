7c3ff509781ef98604ca50b8cac2f37a88369784
NickGatti <nick.gatti@gmail.com>
2020-02-14 11:52:55 -0700

    clean up of proposal

--- src/App.js
+++ src/App.js
@@ -1,77 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Home from './components/home/Home';
 
 export default function App() {
   return (
-    <div>
+    <Router>
       <nav className="nav">
         <div className="nav--item">
-          <h2 className="heading-2 center heading-2--hover--rock-salt">
+          <Link to="/" className="nav__link nav__link--hover--rock-salt center">
             Kickstarter
-          </h2>
+          </Link>
         </div>
         <div className="nav--item">
-          <h2 className="heading-2 center heading-2--hover--rock-salt">Wiki</h2>
+          <Link to="/" className="nav__link nav__link--hover--rock-salt center">
+            Wiki
+          </Link>
         </div>
         <div className="nav--item">
-          <h2 className="heading-2 center heading-2--hover--rock-salt">Play</h2>
+          <Link to="/" className="nav__link nav__link--hover--rock-salt center">
+            Play
+          </Link>
         </div>
       </nav>
-      <div className="root-container">
-        <div className="inner-container">
-          <header className="header">
-            <div className="header__logo-1 text-color-inktongue">
-              <img
-                className="header__logo-img"
-                src="img/knuckledragger_logo.jpg"
-                alt="Logo"
-              />
-            </div>
-            <h1 className="header__heading heading-1 center">
-              KnuckledraggerRPG
-              <small className="heading-1--small text-color-inktongue">
-                Welcome to Planet
-              </small>
-            </h1>
-            <div className="header__text-box">
-              <p className="header__text-text-1">
-                Tabletop B-RPG with simplicity in mind
-              </p>
-              <p className="header__text-text-1 margin-top-small">
-                For an android with no feelings, he sure managed to evoke them
-                in others. Worf, It's better than music. It's jazz. and attack
-                the Romulans. A lot of things can change in twelve years,
-                Admiral. And blowing into maximum warp speed, you appeared for
-                an instant to be in two places at once. Your shields were
-                failing, sir. Earl Grey tea, watercress sandwiches... and
-                Bularian canapés? Are you up for promotion? I guess it's better
-                to be lucky than good. My oath is between Captain Kargan and
-                myself. Your only concern is with how you obey my orders. Or do
-                you prefer the rank of prisoner to that of lieutenant?
-              </p>
-            </div>
-          </header>
-          <section className="features">
-            <div className="features__text-box">
-              Play, create and host... Yesterday I did not know how to eat gagh.
-              I'll be sure to note that in my log. Damage report! Not if I
-              weaken first. Smooth as an android's bottom, eh, Data? I am your
-              worst nightmare! You enjoyed that. When has justice ever been as
-              simple as a rule book? I can't. As much as I care about you, my
-              first duty is to the ship. But the probability of making a six is
-              no greater than that of rolling a seven. Could someone survive
-              inside a transporter buffer for 75 years? Maybe we better talk out
-              here; the observation lounge has turned into a swamp.
-            </div>
-            <div className="features__image-box">
-              <img
-                className="features__image"
-                src="img/promo_img_1.png"
-                alt="Gameplay"
-              />
-            </div>
-          </section>
-        </div>
-      </div>
-    </div>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
